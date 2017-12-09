937ee252792c0355e848759bf2e80adc950165bc
lemuellozada <32891869+lemuellozada@users.noreply.github.com>
2017-12-10 07:50:12 +0800

    Update cachematrix.R
    Programming assignment number 2.
    
    Still pretty confused about what I'm doing. I would really appreciate if you guys can send readings or sources to make me understand this more. Thank you.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Assignment number 3. will invert a matrix using cached data using lexical scoping 
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+                }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, 
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## retrieves the inverse matrix from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+                }
+        data <-x$get()
+        m <- solve(data,...)
+        x$setinverse(m)
+        m                             
+        
 }
